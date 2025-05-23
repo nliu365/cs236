c5ca76a0f6f2d5c7637a88ac5c51f782ea4f7513
Ning Liu <ningliu365@gmail.com>
2025-05-23 00:42:53 -0700

    Update bayes.py by adding spam email checking feature

--- text_classifier/bayes.py
+++ text_classifier/bayes.py
@@ -1,5 +1,5 @@
 '''
-To Use it, 
+To Use it for abusive content check
 >>> import bayes
 >>> listOPosts, listClasses = bayes.loadDataSet()
 >>> myVocabList = bayes.createVocalList(listOPosts)
@@ -24,6 +24,45 @@ def loadDataSet():
   classVec = [0, 1, 0, 1, 0, 1] # 1 is abusive, 0 not
   return postingList, classVec
 
+def textParse(bigString):
+  import re
+  listOfTokens = re.split(r'\W*', bigString)
+  return [tok.lower() for tok in listOfTokens if len(tok) > 2]
+
+
+def spamTest():
+  docList = []
+  classList = []
+  fullText = []
+  for i in range(1, 26):
+    wordList = textParse(open('email/spam/%d.txt' % i).read())
+    docList.append(wordList)
+    fullText.append(wordList)
+    classList.append(1)
+    wordList = testParse(open('email/ham/%d.txt' % i).read())
+    docList.append(wordList)
+    fullTest.extend(wordList)
+    classList.append(0)
+  vocabList = createVocabList(docList)
+  trainingSet = range(50)
+  testSet = []
+  for i in range(10):
+    randIndex = int(random.uniform(0, len(trainingSet)))
+    testSet.append(trainingSet[randIndex])
+    del(trainingSet[randIndex])
+  trainMat = []
+  trainClass = []
+  for docIndex in trainingSet:
+    trainMat.append(setOfWords2Vec(vocalList, docList[docIndex]))
+    trainClasses.append(classList[docIndex])
+  p0V, p1V, pSpam = trainNB0(array(trainMat), array(trainClasses))
+  errorCount = 0
+  for docIndex in testSet:
+    wordVector = setOfWords2Vec(vocalList, docList[docIndex])
+    if classifyNB(array(wordVector), p0V, p1V, pSpam) != classList[docIndex]:
+      errorCount += 1
+  print(f'the error rate is: {float(errorCount) / length(testSet)')
+
 
 def createVocabList(dataSet):
   vocalSet = set([])
@@ -39,6 +78,14 @@ def setOfWords2Vec(vocalList, inputSet):
       returnVec[vocalList.index(word)] = 1
     else:
       print(f'the word {word} is not in my Vocabulary!')
+  return returnVec
+
+
+def bagOfWords2VecMN(vocabList, inputSet):
+  returnVec = [0] * len(vocabList)
+  for word in inputSet:
+    if word in vocabList:
+      returnVec[vocabList.index(word)] += 1
   return returnVec
 
 
